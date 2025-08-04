63d5cf03f5d2bba1a22ef520579342d1c6b63da3
domanh09012006 <doducmanh09012006@gmail.com>
2025-08-04 19:18:42 +0700

    Session02

---
+++ PTIT_CNTT2_IT104_Session02/PTIT_CNTT2_IT104_Session02_Bai7/formatData.js
@@ -0,0 +1,10 @@
+function formatDate(dateInput) {
+    let date = new Date(dateInput);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    if (day < 10) day = "0" + day;
+    if (month < 10) month = "0" + month;
+    return day + "/" + month + "/" + year;
+}
+export { formatDate };
---
+++ PTIT_CNTT2_IT104_Session02/PTIT_CNTT2_IT104_Session02_Bai7/index.js
@@ -0,0 +1,3 @@
+import { formatDate } from './formatData.js';
+let result = formatDate(new Date());
+console.log(result);
